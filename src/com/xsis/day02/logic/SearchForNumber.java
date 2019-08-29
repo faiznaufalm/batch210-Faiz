d9ccc8e73057bc44fd3a7e931a701e8d3fc56e9a
Faiz Naufal M <faiznaufalm@gmail.com>
2019-08-30 01:26:40 +0700

    day02

--- src/com/xsis/day02/logic/SearchForNumber.java
+++ src/com/xsis/day02/logic/SearchForNumber.java
@@ -5,7 +5,7 @@ public class SearchForNumber {
         int[] nums = {1, 5, 4, 43, -2, 6, 4, 9};
         int search = 4;
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] % 4 == 0){
+            if (nums[i]  == 4){
                 System.out.println("Found " + search + " at position " + i);
             }
         }
